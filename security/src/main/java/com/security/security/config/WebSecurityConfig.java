19df91b32b6b85527d67bd4063033622e9c50368
MaZhuli <345957335@qq.com>
2020-09-19 15:59:53 +0800

    test

--- security/src/main/java/com/security/security/config/WebSecurityConfig.java
+++ security/src/main/java/com/security/security/config/WebSecurityConfig.java
@@ -3,9 +3,11 @@ package com.security.security.config;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
